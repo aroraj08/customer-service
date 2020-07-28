0eeb3414f34bdd18be233cbd76716d62bc709418
aroraj08 <arorajatin08@gmail.com>
2020-07-27 18:07:54 -0700

    changes for configuring redis ttl

--- src/main/java/com/example/springjpa/config/CacheConfig.java
+++ src/main/java/com/example/springjpa/config/CacheConfig.java
@@ -1,26 +1,25 @@
 package com.example.springjpa.config;
 
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.support.CompositeCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.redis.cache.RedisCache;
-import org.springframework.data.redis.cache.RedisCacheManager;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
+@ConfigurationProperties("cache")
+@Data
 public class CacheConfig {
 
-    @Bean
-    @Profile("local")
-    public CacheManager cacheManager() {
-        CompositeCacheManager compositeCacheManager = new CompositeCacheManager();
-        compositeCacheManager.setFallbackToNoOpCache(true);
-        return compositeCacheManager;
-    }
+    private long timeoutSeconds;
+    private int redisPort;
+    private String redisHost;
+    private Map<String, Long> cacheExpiration = new HashMap<>();
+
 }
